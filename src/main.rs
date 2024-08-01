100c6e6aa938ec0809942cdff829416c5d9d247f
Kelly Netterville <15361880+scnetter@users.noreply.github.com>
2024-07-31 22:09:20 -0500

    Create full deck of cards

--- src/main.rs
+++ src/main.rs
@@ -6,8 +6,21 @@ struct Deck {
 }
 
 fn main() {
+    // List of suites - 'hearts' 'spades' 'clubs' 'diamonds'
+    // List of values
+    let suits = ["Hearts", "Clubs", "Diamonds", "Spades"];
+    let values = ["Ace", "Two", "Three", "Four", "Five", "Six", "Seven",
+        "Eight", "Nine", "Ten", "Jack", "Queen", "King"];
+    let mut cards = vec![];
+
+    for suit in suits {
+        for value in values {
+            let card = format!("{} of {}", value, suit);
+            cards.push(card);
+        }
+    }
     // Rust Rover doesn't include the type inlays let deck: Deck
-    let deck = Deck {cards: vec![] };
+    let deck = Deck {cards};
     // vec![] is the same as Vec::new
     // :? is a debug formatter
     println!("here comes your deck: {:?}", deck);
