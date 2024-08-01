c76353dacc695286d765e3234b2c00f65af1ed52
Kelly Netterville <15361880+scnetter@users.noreply.github.com>
2024-07-31 20:00:13 -0500

    Initial code commit - Deck struct

---
+++ src/main.rs
@@ -0,0 +1,14 @@
+// Struct - similar to a class in other languages
+// Instances of Deck have different vectors
+#[derive(Debug)]
+struct Deck {
+    cards: Vec<String>  // Vector - ordered list of values that can grow and shrink, arrays are fixed
+}
+
+fn main() {
+    // Rust Rover doesn't include the type inlays let deck: Deck
+    let deck = Deck {cards: vec![] };
+    // vec![] is the same as Vec::new
+    // :? is a debug formatter
+    println!("here comes your deck: {:?}", deck);
+}
